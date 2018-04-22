27f10d44498e18f8a819a5fb4c012e0a5f59f0b8
johnlin08 <1017949384@qq.com>
2018-04-22 21:17:07 +0800

    eureka-client-provider commit

--- eureka-client-provider/src/main/java/com/master/eureka_client_provider/controller/UserController.java
+++ eureka-client-provider/src/main/java/com/master/eureka_client_provider/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.master.eureka_client_provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.master.eureka_client_provider.entity.User;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 @RestController
 public class UserController {
@@ -17,8 +22,28 @@ public class UserController {
 		User user = new User();
 		user.setUserName("Liming");
 		user.setGender("male");
+		System.out.println("-========-");
+
 		return user;
 		
 	}
 
+	 @Autowired
+	  private EurekaClient eurekaClient;
+
+	  @Autowired
+	  private DiscoveryClient discoveryClient;
+
+	
+	  @GetMapping("/eureka-instance")
+	  public String serviceUrl() {
+	    InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+	    return instance.getHomePageUrl();
+	  }
+
+	  @GetMapping("/instance-info")
+	  public ServiceInstance showInfo() {
+	    ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+	    return localServiceInstance;
+	  }
 }
